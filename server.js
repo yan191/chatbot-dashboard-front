9625ab6fafe1d404e25fbbd42448ae93619724a2
Yan Rodrigues <yan.rodrigues.azevedo@hotmail.com>
2019-05-15 12:48:17 -0300

    deploy

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-//Install express server
+//Install express server    
 const express = require('express');
-const path = require('path');
 
-const app = express();
+const path = require('path');   
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/dashboard'));
+const app = express();   
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/dashboard/index.html'));
-});
+// Serve only the static files form the dist directory    
+app.use(express.static(__dirname + '/dist'));
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+app.get('/*', function(req,res) {  
+    res.sendFile(path.join(__dirname+'/dist/index.html'));   
+});  
+
+// Start the app by listening on the default Heroku port    
+app.listen(process.env.PORT || 8080);
