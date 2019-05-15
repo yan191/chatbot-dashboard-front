abeeba2486ca03d8c31685b7fdace301708bddc9
Yan Rodrigues <yan.rodrigues.azevedo@hotmail.com>
2019-05-15 11:40:44 -0300

    heroku deploy

--- server.js
+++ server.js
@@ -7,11 +7,6 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/dashboard'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/dashboard/index.html'));
-});
-
 // Start the app by listening on the default Heroku port
 app.listen( app.get( 'port' ), function() {
     console.log( 'Node server is running on port ' + app.get( 'port' ));
